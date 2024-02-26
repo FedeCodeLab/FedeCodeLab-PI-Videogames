f3f0fb3a9029b719219e0906ff5a32bea34723b2
fedecodelab <federicoguzman.css@gmail.com>
2024-02-26 14:20:32 -0300

    getVideogameByName finalizado

--- api/src/controllers/getApiVideogames.js
+++ api/src/controllers/getApiVideogames.js
@@ -2,65 +2,31 @@ const https = require("https");
 require("dotenv").config();
 const { API_KEY } = process.env;
 
-const getApiVideogames = (req, res) => {
-	let apiGames = [];
-	const pageSize = 40;
-	const totalPages = Math.ceil(100 / pageSize);
+const axios = require("axios");
 
-	// Función para realizar una solicitud GET a la API
-	const fetchPage = (page) => {
-		return new Promise((resolve, reject) => {
-			https
-				.get(
-					`https://api.rawg.io/api/games?key=${API_KEY}&page=${page}&page_size=${pageSize}`,
-					(response) => {
-						let data = "";
+const getApiVideogames = async (req, res) => {
+	try {
+		const pageSize = 40;
+		const totalPages = Math.ceil(100 / pageSize);
+		const apiGames = [];
 
-						// Recibiendo datos
-						response.on("data", (chunk) => {
-							data += chunk;
-						});
-
-						// Fin de la respuesta
-						response.on("end", () => {
-							try {
-								const jsonData = JSON.parse(data);
-								resolve(jsonData.results);
-							} catch (error) {
-								console.error("Error parsing JSON: ", error);
-								reject(error);
-							}
-						});
-					}
-				)
-				.on("error", (error) => {
-					console.error("Error fetching videogames from API: ", error);
-					reject(error);
-				});
-		});
-	};
-
-	// Iterar sobre las páginas y realizar las solicitudes
-	const requests = [];
-	for (let page = 1; page <= totalPages; page++) {
-		requests.push(fetchPage(page));
-	}
-
-	// Esperar a que todas las solicitudes se completen
-	Promise.all(requests)
-		.then((responses) => {
-			// Concatenar los resultados de todas las páginas
-			responses.forEach((data) => {
-				apiGames = apiGames.concat(data);
+		for (let page = 1; page <= totalPages; page++) {
+			const response = await axios.get("https://api.rawg.io/api/games", {
+				params: {
+					key: API_KEY,
+					page: page,
+					page_size: pageSize,
+				},
 			});
-			// Cortar el array para tener solo los primeros 100 juegos
-			apiGames = apiGames.slice(0, 100);
-			res.status(200).json(apiGames);
-		})
-		.catch((error) => {
-			console.error("Error fetching videogames from API: ", error);
-			res.status(500).json({ error: "Internal Server Error" });
-		});
+			apiGames.push(...response.data.results);
+		}
+
+		const slicedGames = apiGames.slice(0, 100);
+		res.status(200).json(slicedGames);
+	} catch (error) {
+		console.error("Error fetching videogames from API: ", error);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 };
 
 module.exports = getApiVideogames;
--- api/src/controllers/getVideogameByName.js
+++ api/src/controllers/getVideogameByName.js
@@ -1,24 +1,59 @@
-const { Op } = require("sequelize");
-const { Videogame, Genre, Platform } = require("../db");
-const getAllVideogames = require("./getAllVideogames"); // Importa el controlador getAllVideogames
+const axios = require("axios");
 require("dotenv").config();
 const { API_KEY } = process.env;
+const { Videogame, Genre, Platform } = require("../db");
+const { Op } = require("sequelize");
 
 const getVideogameByName = async (req, res) => {
 	const { name } = req.query;
 	try {
-		// Obtener todos los videojuegos
-		const allVideogames = await getAllVideogames(req, res);
-
-		// Filtrar los videojuegos por nombre
-		const filteredVideogames = allVideogames.filter((videogame) => {
-			return videogame.name.toLowerCase().includes(name.toLowerCase());
+		// Obtener los videojuegos de la base de datos que coinciden con el nombre
+		const dbVideogames = await Videogame.findAll({
+			where: {
+				name: {
+					[Op.iLike]: `%${name}%`,
+				},
+			},
+			include: [Genre, Platform],
 		});
 
-		if (filteredVideogames.length > 0) {
-			res.status(200).json(filteredVideogames);
+		// Realizar la solicitud a la API Rawg para obtener los videojuegos
+		const pageSize = 40;
+		const totalPages = Math.ceil(100 / pageSize);
+		const apiGames = [];
+
+		for (let page = 1; page <= totalPages; page++) {
+			const response = await axios.get(
+				`https://api.rawg.io/api/games?key=${API_KEY}&page=${page}&page_size=${pageSize}`
+			);
+
+			// Filtrar los juegos de la API que ya están en la base de datos y que no tienen el mismo nombre
+			const filteredGames = response.data.results.filter(
+				(game) =>
+					!dbVideogames.some(
+						(dbGame) => dbGame.name.toLowerCase() === game.name.toLowerCase()
+					)
+			);
+
+			// Filtrar los juegos de la API por nombre
+			const filteredGamesByName = filteredGames.filter((game) =>
+				game.name.toLowerCase().includes(name.toLowerCase())
+			);
+
+			// Agregar los juegos filtrados a apiGames
+			apiGames.push(...filteredGamesByName);
+		}
+
+		// Obtener los primeros 100 juegos
+		const slicedGames = apiGames.slice(0, 100);
+
+		// Combinar los juegos de la base de datos y los juegos de la API
+		const allGames = [...dbVideogames, ...slicedGames];
+
+		if (allGames.length > 0) {
+			res.status(200).json(allGames);
 		} else {
-			res.status(400).json({ message: "No se encontró ningún juego" });
+			res.status(404).json({ message: "No se encontraron juegos" });
 		}
 	} catch (error) {
 		res.status(500).json({ error: error.message });
@@ -26,3 +61,139 @@ const getVideogameByName = async (req, res) => {
 };
 
 module.exports = getVideogameByName;
+
+// const https = require("https");
+// const { Op } = require("sequelize");
+// const { Videogame, Genre, Platform } = require("../db");
+// require("dotenv").config();
+// const { API_KEY } = process.env;
+
+// const getVideogameByName = async (req, res) => {
+// 	const { name } = req.query;
+// 	try {
+// 		const dbVideogames = await Videogame.findAll({
+// 			where: {
+// 				name: {
+// 					[Op.iLike]: `%${name}%`,
+// 				},
+// 			},
+// 			include: [Genre, Platform],
+// 		});
+
+// 		const options = {
+// 			hostname: "api.rawg.io",
+// 			path: `/api/games?search=${name}&key=${API_KEY}&pageSize=15`,
+// 			method: "GET",
+// 		};
+
+// 		const request = https.request(options, (response) => {
+// 			let data = "";
+// 			response.on("data", (chunk) => {
+// 				data += chunk;
+// 			});
+
+// 			response.on("end", () => {
+// 				try {
+// 					const apiVideogamesRaw = JSON.parse(data).results;
+
+// 					// Aplicar filtro por plataforma Xbox
+// 					const filteredApi = apiVideogamesRaw.filter((game) =>
+// 						game.platforms.some((platform) => platform.platform.name === "Xbox")
+// 					);
+
+// 					const combinedResult = [...dbVideogames, ...filteredApi];
+// 					const result = combinedResult.slice(0, 15);
+
+// 					if (result.length > 0) {
+// 						res.status(200).json(result);
+// 					} else {
+// 						res.status(404).json({
+// 							message: `No se encontró ningún videojuego que coincida con: '${name}'.`,
+// 						});
+// 					}
+// 				} catch (error) {
+// 					res.status(500).json({ error: error.message });
+// 				}
+// 			});
+// 		});
+
+// 		request.on("error", (error) => {
+// 			res.status(500).json({ error: error.message });
+// 		});
+
+// 		request.end();
+// 	} catch (error) {
+// 		res.status(500).json({ error: error.message });
+// 	}
+// };
+
+// ? -------------------------------------------------------------
+
+// module.exports = getVideogameByName;
+
+// const https = require("https");
+// const { Op } = require("sequelize");
+// const { Videogame, Genre, Platform } = require("../db");
+// require("dotenv").config();
+
+// const { API_KEY } = process.env;
+
+// const getVideogameByName = async (req, res) => {
+// 	const { name } = req.query;
+// 	try {
+// 		const dbVideogames = await Videogame.findAll({
+// 			where: {
+// 				name: {
+// 					[Op.iLike]: `%${name}%`,
+// 				},
+// 			},
+// 			include: [Genre, Platform],
+// 		});
+
+// 		const options = {
+// 			hostname: "api.rawg.io", // Aquí corregido
+// 			path: `/api/games?search=${name}&key=${API_KEY}&pageSize=15`, // Aquí también corregido
+// 			method: "GET",
+// 		};
+
+// 		const request = https.request(options, (response) => {
+// 			let data = "";
+// 			response.on("data", (chunk) => {
+// 				data += chunk;
+// 			});
+
+// 			response.on("end", () => {
+// 				try {
+// 					const apiVideogamesRaw = JSON.parse(data).results;
+
+// 					const filteredApi = apiVideogamesRaw.filter((game) =>
+// 						game.name.toLowerCase().includes(name.toLowerCase())
+// 					);
+
+// 					const combinedResult = [...dbVideogames, ...filteredApi];
+// 					const result = combinedResult.slice(0, 15);
+
+// 					if (result.length > 0) {
+// 						res.status(200).json(result);
+// 					} else {
+// 						res.status(404).json({
+// 							message: `No se encontró ningún videojuego que coincida con: '${name}'.`,
+// 						});
+// 					}
+// 				} catch (error) {
+// 					res.status(500).json({ error: error.message });
+// 				}
+// 			});
+// 		});
+
+// 		request.on("error", (error) => {
+// 			res.status(500).json({ error: error.message });
+// 		});
+
+// 		request.end();
+// 	} catch (error) {
+// 		res.status(500).json({ error: error.message });
+// 	}
+// };
+
+// module.exports = getVideogameByName;
